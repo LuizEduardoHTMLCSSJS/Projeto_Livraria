f8d6af8f02457d159d83f649972e48042e789c73
Isabella <bella.auad@hotmail.com>
2026-09-09 22:41:01 -0300

    atualiza banco de dados e livros

--- database.sql
+++ database.sql
@@ -1,19 +1,20 @@
-CREATE DATABASE livraria;
+CREATE DATABASE IF NOT EXISTS livraria;
+
 
 USE livraria;
 
-CREATE TABLE autores (
+CREATE TABLE IF NOT EXISTS autores (
     id_autor INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     nacionalidade VARCHAR(50)
 );
 
-CREATE TABLE categorias (
+CREATE TABLE IF NOT EXISTS categorias (
     id_categoria INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE livros (
+CREATE TABLE IF NOT EXISTS livros (
     id_livro INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(150) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
@@ -28,14 +29,14 @@ CREATE TABLE livros (
         REFERENCES categorias(id_categoria)
 );
 
-CREATE TABLE clientes (
+CREATE TABLE IF NOT EXISTS clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     telefone VARCHAR(20)
 );
 
-CREATE TABLE pedidos (
+CREATE TABLE IF NOT EXISTS pedidos (
     id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
     id_cliente INT NOT NULL,
@@ -45,7 +46,7 @@ CREATE TABLE pedidos (
         REFERENCES clientes(id_cliente)
 );
 
-CREATE TABLE itens_pedido (
+CREATE TABLE IF NOT EXISTS itens_pedido (
     id_item INT AUTO_INCREMENT PRIMARY KEY,
     id_pedido INT NOT NULL,
     id_livro INT NOT NULL,
@@ -61,17 +62,24 @@ CREATE TABLE itens_pedido (
 
 INSERT INTO autores (nome, nacionalidade)
 VALUES
-('Machado de Assis', 'Brasileiro'),
-('J. K. Rowling', 'Britânica');
+('Irmãos Grimm', 'Alemã'),
+('J. R. R. Tolkien', 'Britânica'),
+('C. S. Lewis', 'Britânica'),
+('John Green', 'Americana'),
+('Miguel de Cervantes', 'Espanhola');
+
 
 INSERT INTO categorias (nome)
 VALUES
-('Romance'),
 ('Fantasia'),
-('Ficção');
+('Drama'),
+('Clássico');
 
 INSERT INTO livros
-(titulo, autor, preco, imagem)
+(titulo, preco, imagem, id_autor, id_categoria)
 VALUES
-('Dom Casmurro', 'Machado de Assis', 39.90, 'assets/dom_casmurro.jpg'),
-('Harry Potter e a Pedra Filosofal', 'J. K. Rowling', 49.90, 'assets/harry_potter.jpg');
+('Branca de Neve', 29.90, 'assets/livro1.png', 1, 1),
+('O Senhor dos Anéis', 59.90, 'assets/livro2.png', 2, 1),
+('As Crônicas de Nárnia', 44.90, 'assets/livro3.png', 3, 1),
+('A Culpa é das Estrelas', 34.90, 'assets/livro4.png', 4, 2),
+('Dom Quixote', 39.90, 'assets/livro5.png', 5, 3);
